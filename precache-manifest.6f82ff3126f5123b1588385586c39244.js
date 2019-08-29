7163c97f77655988ff4469864fbdb2c1284605f3
jasonellul <jason.j.ellul@gmail.com>
2019-08-29 10:45:00 -0400

    Updates

---
+++ precache-manifest.6f82ff3126f5123b1588385586c39244.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c45f6440aba1353c04c0f7685366e3",
+    "url": "/coop-reports/index.html"
+  },
+  {
+    "revision": "4e39625e40b39ff756d7",
+    "url": "/coop-reports/static/css/main.e82fc3a3.chunk.css"
+  },
+  {
+    "revision": "13f6d069a025c9030290",
+    "url": "/coop-reports/static/js/2.6e0897e8.chunk.js"
+  },
+  {
+    "revision": "4e39625e40b39ff756d7",
+    "url": "/coop-reports/static/js/main.0e98a859.chunk.js"
+  },
+  {
+    "revision": "411a46f007ffe3023e5b",
+    "url": "/coop-reports/static/js/runtime~main.056f13a3.js"
+  },
+  {
+    "revision": "0669b175d61f14582e28aca898ed4967",
+    "url": "/coop-reports/static/media/burloak.0669b175.jpg"
+  },
+  {
+    "revision": "25994c636390eef30cdf2d86d21de431",
+    "url": "/coop-reports/static/media/coffeestain.25994c63.png"
+  },
+  {
+    "revision": "2541cc282e0dfa8d416ab91b1e34b8fa",
+    "url": "/coop-reports/static/media/cork-board.2541cc28.png"
+  },
+  {
+    "revision": "b01475863966da0b0c3b77405f2fd1d6",
+    "url": "/coop-reports/static/media/desk.b0147586.JPG"
+  },
+  {
+    "revision": "8f6a825af78eb8953f54cd8b90bbcfd6",
+    "url": "/coop-reports/static/media/developer.8f6a825a.png"
+  },
+  {
+    "revision": "9763fc1d578c9ecff0a3ad20c91e5f51",
+    "url": "/coop-reports/static/media/goku.9763fc1d.png"
+  },
+  {
+    "revision": "7e93b9c723075ad5544f36127fb407fc",
+    "url": "/coop-reports/static/media/paper.7e93b9c7.png"
+  },
+  {
+    "revision": "8350ae64bcb6d549b927fb0683943bb2",
+    "url": "/coop-reports/static/media/pin.8350ae64.png"
+  },
+  {
+    "revision": "9fb055eb8f4c8c1d8111e7fa7f0bc40c",
+    "url": "/coop-reports/static/media/realistic-paper.9fb055eb.jpg"
+  },
+  {
+    "revision": "4ce476ec9b32683ecd370d7eae8253f3",
+    "url": "/coop-reports/static/media/securcor-mission-shadow.4ce476ec.jpg"
+  }
+]);
